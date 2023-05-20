ceeab5315b1ff62ca6ef58d8336d420f5f6e2df9
VikasWilliam <vikasdenobili@outlook.com>
2023-05-20 12:47:10 +0530

    new push

---
+++ loaderapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"
+
+const App=()=>{
+  return(
+    <div className="container">
+      <h1>Hello World</h1>
+      <button className="btn btn-primary">click me</button>
+    </div>
+  )
+}
+
+export default App;
