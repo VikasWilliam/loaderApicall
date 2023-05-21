460d89049c077272b2cc76eb5e559da0a6e0c23f
VikasWilliam <vikasdenobili@outlook.com>
2023-05-21 12:07:07 +0530

    loader in React

--- loaderapp/src/App.js
+++ loaderapp/src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
+import { useState,useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
-const App=()=>{
-  return(
+const App = () => {
+  const [loading,setLoading]=useState(false);
+  useEffect(() =>{
+    setLoading(true);
+    setTimeout(() =>{
+      setLoading(false);
+    },5000)
+  
+
+  },[]) 
+  return (
     <div className="container">
+    {
+      loading?
+      <ClipLoader
+      color={'#D0021B'}
+      loading={loading}
+      size={150}
+      aria-label="Loading Spinner"
+    /> : 
+    <div >
       <h1>Hello World</h1>
-      <button className="btn btn-primary">click me</button>
+      <div className="row">
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
+      </div>
     </div>
-  )
-}
+    }
+    
+    </div>
+  );
+};
 
 export default App;
